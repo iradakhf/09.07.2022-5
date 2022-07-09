40ffa07cc79b9b01569269ddc17778afb0c3e3dc
iradakhf <iradakhf@code.edu.az>
2022-07-09 08:14:29 +0400

    add ConsoleApp13

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Arrays
+{
+    class Program
+    {
+        public static void Main (string[] args)
+        {
+            int n = 478;
+            DisplayDigitsOfNumber(n);
+        }
+        public static void  DisplayDigitsOfNumber(int number)
+        {
+            int remainder = 0;
+            while (number>0)
+            {
+                remainder = number % 10;
+                number /= 10;
+                Console.WriteLine(remainder);
+            }
+           
+            
+
+        }
+    }
+}
